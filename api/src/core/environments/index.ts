589bee6fb792025f445f75485f885131abf2e0cf
Jailton Cruz <jailtoncruz01@gmail.com>
2023-06-02 22:47:34 -0300

    chore: update build spec

--- api/src/core/environments/index.ts
+++ api/src/core/environments/index.ts
@@ -9,9 +9,11 @@ const file = files.length > 1 ?
   files.find(f => f.includes(process.env.NODE_ENV)) :
   files.find(f => f === ".env");
 
-const envs = readFileSync(resolve(process.cwd(), file))
+const envs = file ? readFileSync(resolve(process.cwd(), file))
   .toString().split("\n").filter(e => !e.startsWith("#") && e.length > 0)
-  .map(line => line.split("=") as [string, string])
+  .map(line => line.split("=") as [string, string]) : []
+
+console.log({ cwd: process.cwd(), files, env: process.env.NODE_ENV })
 
 const environment: ProjectEnvironment = new ProjectEnvironment(envs);
 
